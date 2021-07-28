3d007de8116f236766b545acbaf8480540ca7f8d
Sasha Le <iamsasha.le@gmail.com>
2021-07-28 22:39:40 +1000

    super basic implementation of navbar and upload functionality
    - sidebar GUI
    - Upload files to S3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Router, Route, Switch } from "react-router-dom";
+
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator} from '@aws-amplify/ui-react';
+
+
+import awsExports from "./aws-exports";
+
+import { createBrowserHistory as createHistory } from 'history'
+//import Navbar from 'react-bootstrap/Navbar';
+//import Nav from 'react-bootstrap/Nav';
+
+import Sidebar from './components/Sidebar'
+import UploadMediaFile from './components/pages/UploadFile';
+import HomePage from './components/pages/HomePage';
+
+Amplify.configure(awsExports);
+
+
+const history = createHistory();
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AmplifySignOut></AmplifySignOut>
+      <Router history={history}>
+        <Sidebar/>
+        <Switch>
+          
+          <Route path="/upload-item" component={UploadMediaFile}/>
+          <Route path="/" exact component = {HomePage}/>
+        </Switch>
+      </Router>
     </div>
+    </AmplifyAuthenticator>
+    
   );
 }
 
-export default withAuthenticator(App);
+export default App;
---
+++ src/components/pages/UploadFile.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './UploadFile.css';
+
+import {Storage, API, graphqlOperation } from 'aws-amplify';
+import {createMediaFile } from '../../graphql/mutations';
+import awsExports from '../../aws-exports.js';
+
+/*Once files finishes proccessing pushes to S3, will output message in console 
+Currently no gui prompts if succesfully or not.
+*/
+
+class UploadMediaFile extends React.Component {
+
+    //File stat management
+    constructor(props){
+        super(props);
+        this.state ={
+            file : null
+        }
+    }
+
+    addFileToDB = async (mediaFile) =>{
+        console.log('Adding file to DB')
+        try{
+            await API.graphql(graphqlOperation(createMediaFile, {input:mediaFile}));
+
+        } catch (error){
+            console.log(error)
+        }
+
+    }
+    
+    onChange(e){
+        const file = e.target.files[0];
+        console.log(file);
+
+        Storage.put(file.name, file, {
+            contentType: 'media'
+        }).then((result) => {
+            this.setState({file: URL.createObjectURL(file)})
+            console.log(result);
+            const mediaFile ={
+                name: file.name,
+                file: {
+                    bucket: awsExports.aws_user_files_s3_bucket,
+                    region: awsExports.aws_user_files_s3_bucket_region,
+                    key: 'public/'+ file.name
+                }
+            }
+
+            console.log(mediaFile);
+
+            this.addFileToDB(mediaFile);
+            console.log('file added to database')
+
+        })
+    }
+
+    render(){
+
+        return(
+            <div>
+                <p>Select Local File</p>
+                <input type="file" onChange={(evt) => this.onChange(evt)}/>
+            </div>
+        )
+
+    }
+}
+
+export default UploadMediaFile
