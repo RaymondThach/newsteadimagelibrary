e2d4378ebc214f7cd723556f0e26348133181ba5
james-weier <james.weier1@gmail.com>
2021-08-08 09:09:24 +1000

    test commit

--- src/App.js
+++ src/App.js
@@ -9,74 +9,35 @@ import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Login from "./pages/Login";
 
- 
+
 
 //Import Components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 
-<<<<<<< Updated upstream
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-
-=======
-import Sidebar from './components/Sidebar';
-import UploadMediaFile from './components/pages/UploadFile';
-import HomePage from './components/pages/HomePage';
-
-import Header from './components/Header';
-
-
-Amplify.configure(awsExports);
-
-
-const history = createHistory();
-
-function App() {
-  return (
-    //<AmplifyAuthenticator>
-    
-
-    <div className="App">
-      <img src='/test.png' width="300" height="200"></img>
-      <Header />
       <Router history={history}>
-        <Sidebar/>
->>>>>>> Stashed changes
+        <div>
+        <Sidebar />
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-
           <Route path="/Page1">
             <Page1 />
           </Route>
-
           <Route path="/Page2">
             <Page2 />
           </Route>
-
         </Switch>
-<<<<<<< Updated upstream
-
         <Footer />
       </div>
     </Router>
-  )}
-
-  export default App;
-=======
-      </Router>
-    </div>
-    //</AmplifyAuthenticator>
-    
-  );
+  )
 }
 
+
 export default App;
->>>>>>> Stashed changes
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-<<<<<<< Updated upstream
 import Nav from "./Nav";
-=======
->>>>>>> Stashed changes
 
 
 //Create the header for the webpage
@@ -11,17 +8,14 @@ export default function Header() {
   return (
     <header>
       <br />
-<<<<<<< Updated upstream
      
       <h1>Newstead house Image library</h1>
       <br />
       <Nav />
-=======
 
       <h1 className="webTitle">Newstead House - Image Library</h1>
       <br />
 
->>>>>>> Stashed changes
     </header>
   );
 }
