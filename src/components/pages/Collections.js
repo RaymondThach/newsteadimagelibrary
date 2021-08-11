9e7dd0a236a259f08468c9e7890d1ed305f99417
Fifth05 <81006887+Fifth05@users.noreply.github.com>
2021-08-11 11:59:35 +1000

    Title and Photo Tiles

--- src/components/pages/Collections.js
+++ src/components/pages/Collections.js
@@ -25,23 +25,40 @@ class Collections extends React.Component {
                     return alert ("There was an error creating this colllection: " + err.message);
                 }
                 alert("Successfully created collection.")
-                hi
+
             });
         });
     }*/
     render(){
 
         return(
-            <div className ="main_content">
-                <div class = "header"> 
+            <div class ="title">
                    <div class ="header-center">Collections</div>
                    <div class = "header-right" >
                         <div class ="create-button"onClick={()=>{alert('clicked')}}> + </div>
-                    </div>
+                        </div>   
+                    <div className="main_content">
+            <div class= "responsive">
+                <div class="gallery">
 
+                    <a target= "_blank" href= "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg">
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="BG" width="300" height="200">
+                    </img> 
+                    </a>
+                    <div class="desc">Testing</div>
+
+                    <a target ="_blank" href="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG98ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80">
+                        <img src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG98ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="tester" width="300" height="200">
+                        </img>
+                    </a>
+                    <div class="Desc">tester</div>
                 </div>
-                
             </div>
+            </div>
+            </div>
+
+ 
+                
         )
     }
     
