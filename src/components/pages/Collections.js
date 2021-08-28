0c69c8a4e159ca70a48ba70eac1641fc08f5bfaa
Sasha Le <iamsasha.le@gmail.com>
2021-08-29 01:02:39 +1000

    Merge "Ray" with "Sasha"

--- src/components/pages/Collections.js
+++ src/components/pages/Collections.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Collections.css';
 import CreateCollection from '../Modal/CreateCollection';
 
-import { Storage, API, graphq, graphqlOperation } from 'aws-amplify';
+import {  API, graphqlOperation } from 'aws-amplify';
 import { FcFolder } from "react-icons/fc";
 //import { S3Image } from 'aws-amplify-react-native';
 
